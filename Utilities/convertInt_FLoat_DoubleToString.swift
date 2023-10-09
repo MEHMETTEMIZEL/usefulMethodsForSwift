952de48befa1de484c3d97bcda179487a868d63a
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-09 23:04:45 +0300

    Convert Int, Double, and Float to String Value

--- Utilities/convertInt_FLoat_DoubleToString.swift
+++ Utilities/convertInt_FLoat_DoubleToString.swift
@@ -1,6 +1,26 @@
 import Foundation
 import UIKit
 public class BaseUtils {    
+	
+	/// string değerler null exception hataları engellemek için kullanılır (null -> "")
+	/// - Parameter value: kontrol edilecek Int değeri
+	/// - Returns: parametre nil ise boş string(""), değilse stringe çevirip döndürür.
+	public static func getIntToStringValue( _ value: Int?) -> String {
+		if value == nil {
+			return ""
+		}
+		return String(value!)
+	}
+	
+	/// string değerler null exception hataları engellemek için kullanılır (null -> "")
+	/// - Parameter value: kontrol edilecek Int16 değeri
+	/// - Returns: parametre nil ise boş string(""), değilse stringe çevirip döndürür.
+	public static func getInt16ToStringValue( _ value: Int16?) -> String {
+		if value == nil {
+			return ""
+		}
+		return String(value!)
+	}
 	/// string değerler null exception hataları engellemek için kullanılır (null -> "")
 	/// - Parameter value: kontrol edilecek Int32 değeri
 	/// - Returns: parametre nil ise boş string(""), değilse stringe çevirip döndürür.
