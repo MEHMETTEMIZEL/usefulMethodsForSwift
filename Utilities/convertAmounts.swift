1495a365bee75ef13c3d0a08f832895c6a84f7ff
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-15 09:28:47 +0300

    Added Convert Amount To String Method

--- Utilities/convertAmounts.swift
+++ Utilities/convertAmounts.swift
@@ -37,7 +37,64 @@ extension Utils {
         public static func doubleFromNilableDouble(_ value: Double?) -> Double {
             return Double(value ?? 0)
         }
+		
+		        
+        /// double değeri birimiyle birlikte standart font ayarlarında döndürür
+        /// - Parameters:
+        ///   - amount: string tipinde tutar
+        ///   - fec: para birimi (default: nil)
+        ///   - isBold: textin kalın fontta olup olmaması (default: true)
+        ///   - fontSize: textin font büyüklüğü (default: standartFontSize)
+        ///   - foreColor: textin font rengi  (default: nil)
+        ///   - isSmallFec: tutarın en sağında para biriminin küçük yazılıp yazılmaması (default: true)
+        /// - Returns: verilen parametrelerdeki attributeların uygulandığı NSAttributedString tipinde tutar
+        public static func getAttributedAmountString(amount: String, fec: String? = nil, isBold: Bool? = true, fontSize: CGFloat = Constants.BaseConstants.standartFontSize, foreColor: UIColor? = nil, isSmallFec: Bool? = true) -> NSAttributedString {
+            
+            let amountAttribute = [NSAttributedString.Key.font: Utils.BaseUtils.getFont(isBold!,fontSize)]
+            let fecAttribute = [NSAttributedString.Key.font: Utils.BaseUtils.getFont(isBold!, isSmallFec! ? (fontSize - 5) : fontSize)]
+            
+            let amountText = NSMutableAttributedString()
+            amountText.append(NSMutableAttributedString(string: amount, attributes: amountAttribute ))
+            if let fec = fec{
+                let fecCode = NSMutableAttributedString(string: fec, attributes: fecAttribute)
+                amountText.append(NSMutableAttributedString(string: " ", attributes: amountAttribute))
+                amountText.append(fecCode)
+            }
+            
+            if foreColor != nil {
+                let colorAttributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.foregroundColor: foreColor!]
+                amountText.addAttributes(colorAttributes, range: NSMakeRange(0, amountText.length))
+            }
+            
+            return amountText
+        }
         
+        /// double değerleri currency formatında stringe cevirir (12345.5 -> 12.345,50)
+        /// - Parameters:
+        ///   - value: tutar
+        ///   - fec: para birimi (default: nil)
+        ///   - format: text format tipi (default: n2)
+        ///   - isBold: textin kalın font olup olmaması (default: false)
+        ///   - fontSize: textin font büyüklüğü (default: standartFontSize)
+        ///   - foreColor: textin font rengi  (default: nil)
+        ///   - isSmallFec: tutarın en sağında para biriminin küçük yazılıp yazılmaması (default: true)
+        /// - Returns: verilen parametrelerdeki attributeların uygulandığı NSAttributedString tipinde tutar
+        public static func stringfromAmount( _ value: Double?, fec: String? = nil, format: TextFormatType = .n2, isBold: Bool? = false, fontSize: CGFloat = Constants.BaseConstants.standartFontSize, foreColor: UIColor? = nil, isSmallFec: Bool? = true) -> NSAttributedString {
+            if let val = value {
+
+                let amount = Utils.BaseUtils.getFormatter(type: format).string(from: NSNumber(value: val))!
+                
+                if foreColor == nil {
+                    return Utils.AmountUtils.getAttributedAmountString(amount: amount, fec: fec, isBold: isBold, fontSize: fontSize, isSmallFec: isSmallFec)
+                }else{
+                    return Utils.AmountUtils.getAttributedAmountString(amount: amount, fec: fec, isBold: isBold, fontSize: fontSize, foreColor: foreColor, isSmallFec: isSmallFec)
+                }
+                
+            }
+            else {
+                return NSAttributedString(string: "")
+            }
+        } 
     }
 }
 
