10fcec01373fadf0dbc19798163987a06f7d7450
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-16 12:12:12 +0300

    Arrays advanced features added.

--- SwiftBasics/arraysIntro.swift
+++ SwiftBasics/arraysIntro.swift
@@ -8,3 +8,30 @@ levelDifficulty.isEmpty
 
 //var mostDifficult = levelDifficulty[3]
 levelDifficulty[3] = "Utter Ridiculusness"
+
+
+// Changing & appending items
+var characterClasses = ["Ranger", "Paladin", "Druid"]
+characterClasses.append("Golang")
+characterClasses += ["Healer", "Berserker"]
+
+// Inserting and removing items
+characterClasses.insert("Beast Master", at: 2)
+characterClasses.remove(at: 1)
+
+//Ordering and query,n values
+characterClasses.reverse()
+var reversedClasses = characterClasses.reversed()
+
+characterClasses.sort()
+var sortedClasses = characterClasses.sorted()
+
+print(characterClasses)
+
+// 2D arrays and subscripts
+var skillTree: [[String]] = [
+	["Attack+", "Barrage", "Heavy Hitter"],
+	["Guard+", "Evasion", "Run"]
+]
+
+var attackTreeSkill = skillTree[0][2]
