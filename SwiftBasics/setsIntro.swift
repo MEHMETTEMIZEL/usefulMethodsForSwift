1a4f75cbae649109a5d75279450d8f5df49c2f91
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-16 13:28:08 +0300

    Sets advanced topics added.

--- SwiftBasics/setsIntro.swift
+++ SwiftBasics/setsIntro.swift
@@ -13,3 +13,16 @@ activeQuests.sorted()
 
 
 // NOTE: Sets can only store values that are hashable. -> Meaning They're able to provide a hash value of themselves.
+
+
+
+// Test variables
+var activeQuests: Set = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny Needs Firewood"]
+
+var completeQuests: Set = ["Big Boss", "All-4-One", "The Hereafter"]
+
+// Set operations
+var commonQuests = activeQuests.intersection(completeQuests)
+var differentQuests = activeQuests.symmetricDifference(completeQuests)
+var allQuests = activeQuests.union(completeQuests)
+var clippedQuests = activeQuests.subtracting(completeQuests)
