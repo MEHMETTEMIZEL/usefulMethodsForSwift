abd696559fd75558d6a1d34cdd2ccaae4dc04df9
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-16 12:05:42 +0300

    Swift Basics folder added.

---
+++ SwiftBasics/arraysIntro.swift
@@ -0,0 +1,10 @@
+// Creating arrays
+var levelDifficulty: [String] = ["Easy", "Moderate", "Veteran", "Nightmare"]
+var extraSyntax1: Array<String> = Array<String>()
+
+//Count and isEmpty
+levelDifficulty.count
+levelDifficulty.isEmpty
+
+//var mostDifficult = levelDifficulty[3]
+levelDifficulty[3] = "Utter Ridiculusness"
