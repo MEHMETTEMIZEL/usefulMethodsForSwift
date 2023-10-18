4f2aa041e256f40a697d842b35d577c5e1d40aa7
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-18 08:32:28 +0300

    Overloading functions are added.

--- SwiftBasics/functionsIntro.swift
+++ SwiftBasics/functionsIntro.swift
@@ -9,3 +9,26 @@ func findNearestEnemy(level: Int) -> String {
 }
 
 var enemy = findNearestEnemy(level: 5)
+
+// Overloading Functions
+
+// Base function
+func attackEnemy() {
+	print("Executing default attack...")
+}
+
+// Overloaded functions
+func attackEnemy(damage: Int) {
+	print("Attacking for \(damage)")
+}
+
+func attackEnemy(damage: Double, weapon: String) -> Bool {
+	let attackSuccess = true
+	print("Attacking enemy for \(damage) with \(weapon)")
+	
+	return attackSuccess
+}
+
+attackEnemy()
+attackEnemy(damage: 29)
+var enemyDefeated = attackEnemy(damage: 59.546, weapon: "Deadly Glaive")
