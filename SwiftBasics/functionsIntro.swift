b5bb47c8558356f471df1f27fb18b2b81ac52d68
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-18 08:43:13 +0300

    Complex functions are added.

--- SwiftBasics/functionsIntro.swift
+++ SwiftBasics/functionsIntro.swift
@@ -32,3 +32,35 @@ func attackEnemy(damage: Double, weapon: String) -> Bool {
 attackEnemy()
 attackEnemy(damage: 29)
 var enemyDefeated = attackEnemy(damage: 59.546, weapon: "Deadly Glaive")
+
+
+// Complex Functions
+
+// Optional return value
+func setupArenaMatch() -> Bool? {
+	return nil
+}
+
+if let initSuccess = setupArenaMatch() {
+	print("Level initialized: \(initSuccess)")
+} else {
+	print("Something went terribly wrong...")
+}
+
+// Multiple return values
+func setupArenaMatch(levelName: String) -> (success: Bool, secretItem: String) {
+	print("\(levelName) initialized...")
+	return (true, "Minotaur's Head")
+}
+
+var levelDetails = setupArenaMatch(levelName: "Poison Flats")
+levelDetails.secretItem
+levelDetails.success
+
+// Default values
+func setupDefaultMatch(levelName: String = "Fire Marshes", opponents: Int = 3) {
+	print("Arena match will take place in the \(levelName) between \(opponents) players!")
+}
+
+setupDefaultMatch()
+setupDefaultMatch(levelName: "Poison Flats", opponents: 10)
