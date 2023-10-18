5b9087ee0a5bd215852100c86e68079f9306981a
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-18 08:25:26 +0300

    Functions intro is added.

---
+++ SwiftBasics/functionsIntro.swift
@@ -0,0 +1,11 @@
+// Basic function
+func findNearestAlly() {
+	print("Searching...")
+}
+
+func findNearestEnemy(level: Int) -> String {
+	print("Searching...")
+	return "Argus"
+}
+
+var enemy = findNearestEnemy(level: 5)
