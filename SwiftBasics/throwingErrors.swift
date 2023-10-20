8b5f6ac0737da239aa39ca5b65b940c8783705c1
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-20 22:15:12 +0300

    Handling errors intro added to throw error.

--- SwiftBasics/throwingErrors.swift
+++ SwiftBasics/throwingErrors.swift
@@ -4,6 +4,8 @@ enum DataError {
 	case InvalidPath
 }
 
+let playerDataPath = "/example/data.txt"
+
 // Throwing functions
 func loadData(path: String) throws {
 	guard path.contains("/") else {
@@ -14,4 +16,35 @@ func loadData(path: String) throws {
 		throw DataError.EmptyPath
 	}
 	
+}
+
+//Do-Catch statements
+do {
+	try loadData(path: playerDataPath)
+	print("Data fetch successful!")
+} catch is DataError {
+	print("Invalid or empty path detected...")
+} catch {
+	print("Unknown error detected...")
+}
+
+
+if let dataLoaded = try? loadData(path: playerDataPath) {
+	print("Data fetch went just fine...")
+}
+
+// Propagating errors
+func propagateDataError() throws {
+	try loadData(path: playerDataPath)
+}
+
+do {
+	try propagateDataError()
+	print("Propagated data fetch successful!")
+} catch DataError.EmptyPath {
+	print("Empty path detected...")
+} catch DataError.InvalidPath {
+	print("Invalid path detected...")
+} catch {
+	print("Unknown error detected...")
 }
